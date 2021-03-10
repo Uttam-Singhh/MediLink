4bc5bf98587b9016e1f1248e8c514a6a85e47fb9
Uttam Singh <uttamkhanduja@yahoo.in>
2021-03-10 23:40:57 +0530

    Minor fixes

--- groups/models.py
+++ groups/models.py
@@ -2,16 +2,9 @@ from django.db import models
 from django.conf import settings
 from django.urls import reverse
 from django.utils.text import slugify
-# from accounts.models import User
-
-# pip install misaka
 import misaka
-
 from django.contrib.auth import get_user_model
 User = get_user_model()
-
-# https://docs.djangoproject.com/en/2.0/howto/custom-template-tags/#inclusion-tags
-# This is for the in_group_members check template tag
 from django import template
 register = template.Library()
 
